8c03924cec1a47e8a04a06c50f09b0235e20041a
AbhijeetFadol <46525845+Abhifadol@users.noreply.github.com>
2020-04-11 19:31:02 +0530

    Update readability.py

--- pset6/readability.py
+++ pset6/readability.py
@@ -1,22 +1,42 @@
+# This program calculates the difficulty of a body of text by assigning it a grade
 from cs50 import get_string
-letter=0
-sentence=0
-word=0
-text = get_string("Text: ")
-n = len(text)
-for i in range(n):
-    if text[i].isalnum()==True:
-        letter=letter+1
-for i in range(n):
-    if text[i].isspace()==True and text[i+1].isalnum()==True:
-        word=word+1
-for i in range(n):
-    if text[i]=="?" or text[i]=="." or text[i]=="!":
-        sentence=sentence+1
-grade = round (0.0588 * ((100 * letter) / word) - 0.296 * ((100 * sentence) / word) - 15.8)
-if grade<1:
+
+# gets user input about the text
+text = get_string("Text: \n")
+
+letters = 0
+words = 0
+sentences = 0
+counter = 0
+
+for i in text:
+    counter += 1
+
+for i in range(counter):
+    # counts the letters using ascii code
+    if (ord(text[i]) >= 65 and ord(text[i]) <= 122):
+        letters += 1
+
+    # counts the words by reading spaces
+    elif (ord(text[i]) == 32 and (ord(text[i - 1]) != 33 and ord(text[i - 1]) != 46 and ord(text[i - 1]) != 63)):
+        words += 1
+
+    # counts the sentences by finding dots, exclamation marks and interrogatives
+    elif (ord(text[i]) == 33 or ord(text[i]) == 46 or ord(text[i]) == 63):
+        sentences += 1
+        words += 1
+
+L = letters * 100 / words
+S = sentences * 100 / words
+# Coleman-Liau index is computed using the formula
+index = round(0.0588 * L - 0.296 * S - 15.8)
+
+# Finally outputs the result to the user
+if (index < 1):
     print("Before Grade 1")
-elif grade < 16:
-    print(f"Grade {grade-1}")
-else :
-    print("Grade 16+");
+
+elif (index >= 16):
+    print("Grade 16+")
+
+else:
+    print(f"Grade {index}")
